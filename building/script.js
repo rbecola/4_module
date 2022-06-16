dfda5395b91f8b76f82ff9a5ed8957608e4c9096
rbecola <91329713+rbecola@users.noreply.github.com>
2022-06-16 17:23:48 +0300

    Create script.js

---
+++ building/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+  document.addEventListener('mousemove', e => {
+      circle.style.left = e.pageX -125 + "px";
+      circle.style.top = e.pageY -125 + "px";
+  });
+
+  // $(".back").mouseover(function() {
+  //   $(this).addClass("tr-over");
+  //   $(".news").addClass("add_white");
+  // });
+  // $(".back").mouseout(function() {
+  //   $(this).removeClass("tr-over");
+  //   $(".news").removeClass("add_white"); });
+  //
+
+  $(".about, .building, .agents, .news, .list, .request, .ver1_1, .ver1_2, .ver1_3, .ver1_4, .ver1_5, .ver1_6, .ver1_7, .ver1_8").mouseover(function() {
+    $(".building").addClass("add_grey");
+  });
+  $(".about, .building, .agents, .news, .list, .request, .ver1_1, .ver1_2, .ver1_3, .ver1_4, .ver1_5, .ver1_6, .ver1_7, .ver1_8").mouseout(function() {
+    $(".building").removeClass("add_grey");
+  });
+
+});
+// function changeItem() {
+//   document.getElementById('circle').style.background = '#FF4A4A';
+// }// при наведении круг под курсором меняет оттенок красного.
+//
+// function rechangeItem() {
+//   document.getElementById('circle').style.background = '#FF0000';
+// }// при отводе круг под курсором возвращает свой цвет.
